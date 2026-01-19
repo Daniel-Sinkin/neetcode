a7eef80b269b3fab64ec5dac2d0c4cdf3a58f35a
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-01-19 17:49:39 +0100

    Added precompiled header

---
+++ src/pch.hpp
@@ -0,0 +1,14 @@
+// src/pch.hpp
+#pragma once
+
+// IWYU pragma: keep
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstddef>
+#include <cstdlib>
+#include <print>
+#include <string>
+#include <utility>
+#include <vector>
